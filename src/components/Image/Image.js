6e81ec365c026e56f323445bd6e71e13037e2ed4
dibrahex <danial.ibraheem@gmail.com>
2018-08-20 23:53:02 +0300

    Finished working on Clone

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -16,7 +16,9 @@ class Image extends React.Component {
       size: 200
     };
     this.state = {filter: 0};
+    this.state = {copy: 1};
     this.RandFilter = this.RandFilter.bind(this);
+    this.Duplicate = this.Duplicate.bind(this);
   }
 
   calcImageSize() {
@@ -37,14 +39,20 @@ class Image extends React.Component {
     return `https://farm${dto.farm}.staticflickr.com/${dto.server}/${dto.id}_${dto.secret}.jpg`;
   }
   
-  //set state random number 
+  //set state random number
   RandFilter() {
-    var rand=Math.floor(Math.random() * 5) + 1  
+    var rand=Math.floor(Math.random() * 5) + 1 ;
     this.setState({filter: rand});
   }
-
+  
+  Duplicate() {
+    this.setState({
+      copy: this.state.copy + 1
+    });
+  }
+  
   render() {
-    //Add basic class and add random class on filter click 
+    //Add basic class and add random class on filter click
     let classes = 'image-root ';
    
     switch (this.state.filter) {
@@ -65,10 +73,9 @@ class Image extends React.Component {
     case 5:
          classes += ' RandF5';
         break;
-}
-    return (
-      
-      <div
+  }
+  const children = [];
+  const ChildComponent  = props => ( <div
         className={classes}
         style={{
           backgroundImage: `url(${this.urlFromDto(this.props.dto)})`,
@@ -82,6 +89,19 @@ class Image extends React.Component {
           <FontAwesome className="image-icon" name="expand" title="expand"/>
         </div>
       </div>
+      );
+
+      
+      for (var i = 0; i < this.state.copy; i += 1) {
+       children.push(<ChildComponent key={i} number={i} />);
+      }
+
+
+    return (
+      <div style={{display:'inline-block'}}>
+       {children}
+         </div>
+      
     );
   }
 }
